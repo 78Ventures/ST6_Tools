fda408ff86e084e7c15f50392b2cd897bcc1b760
Doug Daulton <doug.daulton@gmail.com>
2024-05-27 20:40:06 -0700

    0.93: Style Lock
    Finished CSS Style work

--- functions/RENDER/ML_Render_HTML.py
+++ functions/RENDER/ML_Render_HTML.py
@@ -12,13 +12,16 @@ def drive_Output_HTML(target_data):
     <html>
     <head><title>Mileage Log Report</title></head>
     <body style="font-family: 'Open Sans', sans-serif;">
-    <h1>Mileage Log Report</h1>
     <table border="1" style="border-collapse: collapse; width: 100%;">
-    <tr style="background-color: #000000; color: #FFFFFF; vertical-align: middle;">
+      <tr style="vertical-align: middle; font-size:10pt; font-weight: bold;" border="0">
+        <td colspan="3" style="padding: 5px; text-align: left; width: 70%;" border="0">Train Dream: Mileage Report</td>
+        <td colspan="1" style="padding: 5px; text-align: right; width: 30%;" border="0">Doug Daulton</td>
+    </tr>
+    <tr style="background-color: #000000; color: #FFFFFF; vertical-align: middle; font-size:11pt">
         <th style="padding: 5px; text-align: center; width: 10%;">DATE</th>
         <th style="padding: 5px; text-align: center; width: 10%;">MILES</th>
-        <th style="padding: 5px;">Route</th>
-        <th style="padding: 5px;">Notes On Stops</th>
+        <th style="padding: 5px; text-align: left;">ROUTE</th>
+        <th style="padding: 5px; text-align: left;">STOP DETAILS</th>
     </tr>
     """
 
@@ -27,25 +30,31 @@ def drive_Output_HTML(target_data):
         style = row_styles[index % 2]
         html_content += f"""
         <tr style="{style} vertical-align: top;">
-            <td style="padding: 5px; width: 10%;">{row[0]}</td>
-            <td style="padding: 5px; width: 10%; text-align: center;">{row[1]:.2f}</td>
-            <td style="padding: 5px;">{row[2]}</td>
-            <td style="padding: 5px;">{row[3]}</td>
+            <td style="padding: 5px; width: 10%; vertical-align: top; text-align: center;">{row[0]}</td>
+            <td style="padding: 5px; width: 10%; vertical-align: top; text-align: center; font-weight: bold; font-size: 11pt;">{row[1]:.2f}</td>
+            <td style="padding: 5px; width: 50%; vertical-align: top; line-height: 1.1">{row[2]}</td>
+            <td style="padding: 5px; width: 30%; vertical-align: top; line-height: 1.1">{row[3]}</td>
         </tr>
         """
 
     total_miles = sum(row[1] for row in target_data)
     html_content += f"""
-    <tr>
+    <tr id="Total_Miles" style="background-color: #000000; color: #FFFFFF; vertical-align: middle;font-size: 18pt;">
         <td colspan="4" style="text-align: center; font-weight: bold;">Total Miles Driven: {total_miles:.2f}</td>
     </tr>
-    <tr>
-        <td colspan="4" style="text-align: left;">
-            <h2>NOTES:</h2>
+    <tr id="Notes_Key">
+        <td colspan="2" style="text-align: left;padding: 5px; width: 50%;">
+            <strong>KEY</strong>
             <ul>
-                <li>DATE is linked to a Google Maps Itinerary.</li>
-                <li>Each waypoint in the route is linked to a Google Pin for that address.</li>
-                <li>KEY: PC = Picture Cars. LM = Location Management</li>
+                <li><strong>PC:</strong> Picture Cars</li>
+                <li><strong>LS:</strong> Location Scout</li>
+            </ul>
+        </td>
+        <td colspan="2" style="text-align: left;padding: 5px; width: 50%;">
+            <strong>NOTES</strong>
+            <ul>
+                <li><strong>DATE:</strong> Links to a Google Maps Itinerary for the Route.</li>
+                <li><strong>ROUTE:</strong> Each waypoint is linked to a Google Pin for that address.</li>
             </ul>
         </td>
     </tr>
